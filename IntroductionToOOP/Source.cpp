f12d9829eb046b8239c5ddcece31a37f56ec3757
KseniyaPavlova <101828897+KseniyaPavlova@users.noreply.github.com>
2022-05-16 22:17:10 +0300

    Constructors

--- IntroductionToOOP/Source.cpp
+++ IntroductionToOOP/Source.cpp
@@ -5,6 +5,7 @@ using std::cin;
 using std::cout;
 using std::endl;
 
+#define delimiter "\n------------------------------------------\n"
 class Point
 {
 	double x;
@@ -28,17 +29,43 @@ public:
 	}
 
 	//			Constructors:
-	Point()
+	/*Point()
 	{
 		x = y = 0;
 		cout << "DefaultConstructor:\t" << this << endl;
 	}
+	Point(double x)
+	{
+		this->x = x;
+		this->y = 0;
+		cout << "Single argument constructor:\t" << this << endl;
+	}*/
+	Point(double x=0, double y=0)
+	{
+		this->x = x;
+		this->y = y;
+		cout << "Constructor:\t" << this << endl;
+	}
+	Point(const Point& other)
+	{
+		this->x = other.x;
+		this->y = other.y;
+		cout << "CopyConstructor:" << this << endl;
+	}
 	~Point()
 	{
-		cout << "Destructor:\t\t" << this << endl;
+		cout << "Destructor:\t" << this << endl;
+	}
+	//			Operators:
+	Point operator =(const Point& other)
+	{
+		this->x = other.x;
+		this->y = other.y;
+		cout << "CopyAssignment:\t" << this << endl;
+		return *this;
 	}
 	//			Methods
-	double distance(Point other)
+	double distance(const Point& other)
 	{
 		//this (эта точка) - точка, для которой вызвался метод 
 		//       (от которой нужно найти расстояние)
@@ -54,10 +81,11 @@ public:
 	}
 };
 
-double distance(Point A, Point B)
+double distance(const Point& A, const Point& B)
 {
 	double x_distance = A.get_x() - B.get_x();
 	double y_distance = A.get_y() - B.get_y();
+	//A.set_x(-10);
 	double distance = sqrt(x_distance*x_distance + y_distance*y_distance);
 	return distance;
 }
@@ -65,6 +93,9 @@ double distance(Point A, Point B)
 
 //#define STRUCT_POINT
 //#define DISTANCE
+//#define CONSTRUCTORS_CHECK
+//#define ASSIGNMENT_CHECK_1
+#define ASSIGNMENT_CHECK_2
 
 void main()
 {
@@ -77,7 +108,7 @@ void main()
 	A.y = 3;
 	cout << A.x << "\t" << A.y << endl;
 
-	Point* pA = &A;
+	Point* pA = &A; // объявляем указатель на Point и берем адрес точки 'А'
 	cout << pA->x << "\t" << pA->y << endl;
 #endif // STRUCT_POINT
 
@@ -90,16 +121,53 @@ void main()
 	B.set_x(7);
 	B.set_y(8);
 	cout << B.get_x() << "\t" << B.get_y() << endl;
-
+	cout << delimiter << endl;
 	cout << "Расстояние от точки А до точки В: " << A.distance(B) << endl;
+	cout << delimiter << endl;
 	cout << "Расстояние от точки B до точки A: " << B.distance(A) << endl;
+	cout << delimiter << endl;
 	cout << "Расстояние между точками A и В: " << distance(A, B) << endl;
+	cout << delimiter << endl;
 	cout << "Расстояние между точками B и A: " << distance(B, A) << endl;
+	cout << delimiter << endl;
 #endif // DISTANCE
 	 
-	int a;
+#ifdef CONSTRUCTORS_CHECK
+	int a(2);
+	cout << a << endl;
 
 	Point A;
 	//cout << A.get_x() << "\t" << A.get_y() << endl;
 	A.print();
+
+	Point B = 5;  //Single Aegument Constructor
+	B.print();
+
+	Point C(8); //Single Aegument Constructor
+	C.print();
+
+	Point D(2, 3);
+	D.print();
+
+	Point E = D; //Copy constructor
+	E.print();
+
+	Point F(D); //Copy constructor
+	F.print();
+#endif // CONSTRUCTORS_CHECK
+
+#ifdef ASSIGNMENT_CHECK_1
+	//CopyAssignment
+	Point A(2, 3);
+	Point B = A; //CopyConstructor потому что объект 'B' создается 
+	Point C; // Default Constructor
+	C = B; //Copy Assignment потому что объект 'C' уже существует  
+#endif // ASSIGNMENT_CHECK_1
+
+	int a, b, c;
+		a = b = c = 0;
+	cout << a << "\t" << b << "\t" << c << endl;
+
+	Point A, B, C;
+	A = B = C = Point(2, 3);
 }
